7c1cab16b4b5c3885e3cfe9f4b6fd9ddd6187e8b
Lili Rodriguez <lilimrod@gmail.com>
2020-02-03 03:19:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function holidayCountdown(holiday,days){
+  return `It's ${days} days until ${holiday}!`
+}
